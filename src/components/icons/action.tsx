a0d39add991d980fc1b8f58ee822029ebddd3db2
zakevicius <m.zakevicius@gmail.com>
2021-05-23 14:39:05 +0300

    reload window after action

--- src/components/icons/action.tsx
+++ src/components/icons/action.tsx
@@ -1,5 +1,4 @@
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import LockOpenIcon from '@material-ui/icons/LockOpen'
+import { LockOutlined, LockOpen } from '@material-ui/icons'
 import { useSnackbar } from 'notistack'
 
 import { request, throttle } from '../utils'
@@ -7,8 +6,8 @@ import { request, throttle } from '../utils'
 import TooltipWrapper from '../tooltip-wrapper'
 
 const icons = {
-  unclaim: <LockOpenIcon />,
-  claim: <LockOutlinedIcon />
+  unclaim: <LockOpen />,
+  claim: <LockOutlined />
 }
 
 interface Props {
@@ -16,7 +15,9 @@ interface Props {
   action: keyof typeof icons
 }
 
-
+// If claiming would happer during deploy with 'cap deploy', 
+// wouldn't it be good to write additional 'cap unclaim' so we could track user
+// and avoid 'accidental anonymous' unclaiming
 const Action = ({ id, action }: Props) => {
   const { enqueueSnackbar } = useSnackbar()
 
@@ -24,8 +25,9 @@ const Action = ({ id, action }: Props) => {
     try {
       await request("PUT", { id, action })
       enqueueSnackbar(`${action}ed #${id}`, { variant: 'success' })
+      window.location.reload()
     } catch (err) {
-      let message = err.response.data?.error || err.message
+      let message = err.response?.data?.error || err.message
       enqueueSnackbar(message, { variant: 'error' })
     }
   }
